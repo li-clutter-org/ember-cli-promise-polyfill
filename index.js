6515a8f39ae0adee37804e3c49e0807c3c66ddeb
Pepe Cano <ppcanodehuelva@gmail.com>
2018-11-08 11:13:40 +0100

    Check for Ember version 3.4.0 or above

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 'use strict';
 const path = require('path');
 const caniuse = require('caniuse-api');
+const VersionChecker = require('ember-cli-version-checker');
+
+const packageName = require('./package').name;
 
 module.exports = {
-  name: require('./package').name,
+  name: packageName,
 
   importTransforms: require('ember-cli-cjs-transform').importTransforms,
 
-  //isDevelopingAddon() { return true; },
-  //
+  init() {
+    this._super.init.apply(this, arguments);
+
+    const checker = new VersionChecker(this);
+    const ember = checker.forEmber();
+
+    const expectedVersion = '3.4.0';
+
+    if (ember.lt(expectedVersion)) {
+      const error = `
+You must have Ember ${expectedVersion} or above to use "${packageName}".
+Please, uninstall the "${packageName}" addon. Apps using previous Ember versions should continue using "RSVP.Promise" instead of "Native Promises".`;
+
+      throw new Error(error);
+    }
+  },
+
   shouldImportPolyfill() {
     let browsers = this.project.targets && this.project.targets.browsers;
     return !browsers || !caniuse.isSupported('promises', browsers);
